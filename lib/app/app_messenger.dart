11f77263177ad31b152b18514198ed75d0fcb534
KovaKreative <eric.kovalevskyy@gmail.com>
2025-06-17 13:25:40 -0400

    refactor app messenger

--- lib/app/app_messenger.dart
+++ lib/app/app_messenger.dart
@@ -5,16 +5,16 @@ class AppMessenger {
 
   static void show(String message) {
     final messenger = messengerKey.currentState;
-    if (messenger != null) {
-      messenger.showSnackBar(
-        SnackBar(
-          content: Text(message),
-          showCloseIcon: true,
-          dismissDirection: DismissDirection.horizontal,
-        ),
-      );
-    } else {
+    if (messenger == null) {
       debugPrint("⚠️ Tried to show SnackBar, but messenger was null");
+      return;
     }
+    messenger.showSnackBar(
+      SnackBar(
+        content: Text(message),
+        showCloseIcon: true,
+        dismissDirection: DismissDirection.horizontal,
+      ),
+    );
   }
 }
